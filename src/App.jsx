d84208896964a0fbbfbf700c65da268d11a0d16d
md-ahmodullah <contactme.ahmod@gmail.com>
2024-10-18 03:13:52 +0600

    Added Full TaskBar Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
+import TaskBar from "./task/TaskBar";
 
 export default function App() {
   return (
     <>
       <Header />
       <HeroSection />
+      <TaskBar />
       <Footer />
     </>
   );
