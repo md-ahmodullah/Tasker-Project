ed0e72c64242a9e0e2572ea186ce51455319a5f0
md-ahmodullah <contactme.ahmod@gmail.com>
2024-10-18 04:18:09 +0600

    Added Dynamic Task By Default Data

--- src/task/TaskBar.jsx
+++ src/task/TaskBar.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 export default function TaskBar() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "React is used to build single-page applications. React allows us to create reusable UI components.",
+    tags: ["Web", "React", "JS"],
+    priority: "High",
+    isFavourite: false,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -11,7 +22,7 @@ export default function TaskBar() {
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction />
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
